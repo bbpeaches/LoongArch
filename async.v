6e09fe69af0db282814f2bcc74f6fb5afa0346f0
周珈民 <3230715573@qq.com>
2026-07-26 14:17:30 +0800

    freq_divider

--- async.v
+++ async.v
@@ -200,7 +200,7 @@ function integer log2(input integer v); begin log2=0; while(v>>log2) log2=log2+1
 localparam AccWidth = log2(ClkFrequency/Baud)+8;  // +/- 2% max timing error over a byte
 reg [AccWidth:0] Acc = 0;
 localparam ShiftLimiter = log2(Baud*Oversampling >> (31-AccWidth));  // this makes sure Inc calculation doesn't overflow
-localparam Inc = ((Baud*Oversampling << (AccWidth-ShiftLimiter))+(ClkFrequency>>(ShiftLimiter+1)))/(ClkFrequency>>ShiftLimiter);
+localparam [63:0] Inc = ((64'd1 * Baud * Oversampling << (AccWidth-ShiftLimiter))+(ClkFrequency>>(ShiftLimiter+1)))/(ClkFrequency>>ShiftLimiter);
 always @(posedge clk) if(enable) Acc <= Acc[AccWidth-1:0] + Inc[AccWidth:0]; else Acc <= Inc[AccWidth:0];
 assign tick = Acc[AccWidth];
 endmodule
--- thinpad_top.v
+++ thinpad_top.v
@@ -44,23 +44,175 @@ module thinpad_top(
     output wire video_de           
 );
 
-/* =========== Core Logic Begin =========== */
-
 wire locked, clk_cpu, clk_sram;
 pll_example clock_gen (
   .clk_in1(clk_50M),
-  .clk_out1(clk_cpu), 
-  .clk_out2(clk_sram),
+  .clk_out1(clk_cpu),   
+  .clk_out2(clk_sram),  
   .reset(reset_btn),
   .locked(locked)
 );
 
-reg reset_of_clk10M;
-always@(posedge clk_cpu or negedge locked) begin
-    if(~locked) reset_of_clk10M <= 1'b1;
-    else        reset_of_clk10M <= 1'b0;
+// 分别为两个时钟域生成同步复位信号
+reg cpu_reset_sync;
+always @(posedge clk_cpu or negedge locked) begin
+    if (~locked) cpu_reset_sync <= 1'b0;
+    else         cpu_reset_sync <= 1'b1;
 end
-wire cpu_resetn = ~reset_of_clk10M;
+wire cpu_resetn = cpu_reset_sync;
+
+reg sram_reset_sync;
+always @(posedge clk_sram or negedge locked) begin
+    if (~locked) sram_reset_sync <= 1'b0;
+    else         sram_reset_sync <= 1'b1;
+end
+wire sram_resetn = sram_reset_sync;
+
+wire [ 3:0] arid_cpu, arlen_cpu, arcache_cpu, awid_cpu, awlen_cpu, awcache_cpu, wid_cpu, wstrb_cpu, rid_cpu, bid_cpu;
+wire [31:0] araddr_cpu, rdata_cpu, awaddr_cpu, wdata_cpu;
+wire [ 2:0] arsize_cpu, arprot_cpu, awsize_cpu, awprot_cpu;
+wire [ 1:0] arburst_cpu, arlock_cpu, awburst_cpu, awlock_cpu, rresp_cpu, bresp_cpu;
+wire arvalid_cpu, arready_cpu, rlast_cpu, rvalid_cpu, rready_cpu;
+wire awvalid_cpu, awready_cpu, wlast_cpu, wvalid_cpu, wready_cpu;
+wire bvalid_cpu, bready_cpu;
+
+mycpu_top u_cpu (
+    .aclk       (clk_cpu),
+    .aresetn    (cpu_resetn),
+    
+    .arid       (arid_cpu),
+    .araddr     (araddr_cpu),
+    .arlen      (arlen_cpu),
+    .arsize     (arsize_cpu),
+    .arburst    (arburst_cpu),
+    .arlock     (arlock_cpu),
+    .arcache    (arcache_cpu),
+    .arprot     (arprot_cpu),
+    .arvalid    (arvalid_cpu),
+    .arready    (arready_cpu),
+    
+    .rid        (rid_cpu),
+    .rdata      (rdata_cpu),
+    .rresp      (rresp_cpu),
+    .rlast      (rlast_cpu),
+    .rvalid     (rvalid_cpu),
+    .rready     (rready_cpu),
+    
+    .awid       (awid_cpu),
+    .awaddr     (awaddr_cpu),
+    .awlen      (awlen_cpu),
+    .awsize     (awsize_cpu),
+    .awburst    (awburst_cpu),
+    .awlock     (awlock_cpu),
+    .awcache    (awcache_cpu),
+    .awprot     (awprot_cpu),
+    .awvalid    (awvalid_cpu),
+    .awready    (awready_cpu),
+    
+    .wid        (wid_cpu),
+    .wdata      (wdata_cpu),
+    .wstrb      (wstrb_cpu),
+    .wlast      (wlast_cpu),
+    .wvalid     (wvalid_cpu),
+    .wready     (wready_cpu),
+    
+    .bid        (bid_cpu),
+    .bresp      (bresp_cpu),
+    .bvalid     (bvalid_cpu),
+    .bready     (bready_cpu),
+    
+    .debug_wb_pc(), .debug_wb_rf_we(), .debug_wb_rf_wnum(), .debug_wb_rf_wdata()
+);
+
+wire [ 3:0] arid_sram, arlen_sram, arcache_sram, awid_sram, awlen_sram, awcache_sram, wid_sram, wstrb_sram, rid_sram, bid_sram;
+wire [31:0] araddr_sram, rdata_sram, awaddr_sram, wdata_sram;
+wire [ 2:0] arsize_sram, arprot_sram, awsize_sram, awprot_sram;
+wire [ 1:0] arburst_sram, arlock_sram, awburst_sram, awlock_sram, rresp_sram, bresp_sram;
+wire arvalid_sram, arready_sram, rlast_sram, rvalid_sram, rready_sram;
+wire awvalid_sram, awready_sram, wlast_sram, wvalid_sram, wready_sram;
+wire bvalid_sram, bready_sram;
+
+axi_cdc_wrapper #(
+    .USE_CDC(0) // 1: 启用 IP 0: 直通不打拍
+) u_axi_cdc_wrapper (
+    // Slave 侧 (接 CPU)
+    .s_axi_aclk    (clk_cpu),
+    .s_axi_aresetn (cpu_resetn),
+    .s_axi_arid    (arid_cpu),
+    .s_axi_araddr  (araddr_cpu),
+    .s_axi_arlen   (arlen_cpu),
+    .s_axi_arsize  (arsize_cpu),
+    .s_axi_arburst (arburst_cpu),
+    .s_axi_arlock  (arlock_cpu),
+    .s_axi_arcache (arcache_cpu),
+    .s_axi_arprot  (arprot_cpu),
+    .s_axi_arvalid (arvalid_cpu),
+    .s_axi_arready (arready_cpu),
+    .s_axi_rid     (rid_cpu),
+    .s_axi_rdata   (rdata_cpu),
+    .s_axi_rresp   (rresp_cpu),
+    .s_axi_rlast   (rlast_cpu),
+    .s_axi_rvalid  (rvalid_cpu),
+    .s_axi_rready  (rready_cpu),
+    .s_axi_awid    (awid_cpu),
+    .s_axi_awaddr  (awaddr_cpu),
+    .s_axi_awlen   (awlen_cpu),
+    .s_axi_awsize  (awsize_cpu),
+    .s_axi_awburst (awburst_cpu),
+    .s_axi_awlock  (awlock_cpu),
+    .s_axi_awcache (awcache_cpu),
+    .s_axi_awprot  (awprot_cpu),  
+    .s_axi_awvalid (awvalid_cpu),
+    .s_axi_awready (awready_cpu),
+    .s_axi_wdata   (wdata_cpu),
+    .s_axi_wstrb   (wstrb_cpu),
+    .s_axi_wlast   (wlast_cpu),
+    .s_axi_wvalid  (wvalid_cpu),
+    .s_axi_wready  (wready_cpu),
+    .s_axi_bid     (bid_cpu),
+    .s_axi_bresp   (bresp_cpu),
+    .s_axi_bvalid  (bvalid_cpu),
+    .s_axi_bready  (bready_cpu),
+
+    // Master 侧 (接 SRAM/UART 外设状态机)
+    .m_axi_aclk    (clk_sram),
+    .m_axi_aresetn (sram_resetn),
+    .m_axi_arid    (arid_sram),
+    .m_axi_araddr  (araddr_sram),
+    .m_axi_arlen   (arlen_sram),
+    .m_axi_arsize  (arsize_sram),
+    .m_axi_arburst (arburst_sram),
+    .m_axi_arlock  (arlock_sram),
+    .m_axi_arcache (arcache_sram),
+    .m_axi_arprot  (arprot_sram),
+    .m_axi_arvalid (arvalid_sram),
+    .m_axi_arready (arready_sram),
+    .m_axi_rid     (rid_sram),
+    .m_axi_rdata   (rdata_sram),
+    .m_axi_rresp   (rresp_sram),
+    .m_axi_rlast   (rlast_sram),
+    .m_axi_rvalid  (rvalid_sram),
+    .m_axi_rready  (rready_sram),
+    .m_axi_awid    (awid_sram),
+    .m_axi_awaddr  (awaddr_sram),
+    .m_axi_awlen   (awlen_sram),
+    .m_axi_awsize  (awsize_sram),
+    .m_axi_awburst (awburst_sram),
+    .m_axi_awlock  (awlock_sram),
+    .m_axi_awcache (awcache_sram), 
+    .m_axi_awprot  (awprot_sram),
+    .m_axi_awvalid (awvalid_sram),
+    .m_axi_awready (awready_sram),
+    .m_axi_wdata   (wdata_sram),
+    .m_axi_wstrb   (wstrb_sram),
+    .m_axi_wlast   (wlast_sram),
+    .m_axi_wvalid  (wvalid_sram),
+    .m_axi_wready  (wready_sram),
+    .m_axi_bid     (bid_sram),
+    .m_axi_bresp   (bresp_sram),
+    .m_axi_bvalid  (bvalid_sram),
+    .m_axi_bready  (bready_sram)
+);
 
 wire [7:0] ext_uart_rx;
 reg  [7:0] ext_uart_buffer;
@@ -69,7 +221,7 @@ wire ext_uart_ready, ext_uart_clear, ext_uart_busy;
 reg  ext_uart_start, ext_uart_avai;
 
 async_receiver #(.ClkFrequency(50000000), .Baud(115200)) ext_uart_r (
-    .clk(clk_cpu), 
+    .clk(clk_sram), 
     .RxD(rxd),
     .RxD_data_ready(ext_uart_ready),
     .RxD_clear(ext_uart_clear),
@@ -78,96 +230,39 @@ async_receiver #(.ClkFrequency(50000000), .Baud(115200)) ext_uart_r (
 
 assign ext_uart_clear = ext_uart_ready; 
 async_transmitter #(.ClkFrequency(50000000), .Baud(115200)) ext_uart_t (
-    .clk(clk_cpu), 
+    .clk(clk_sram), 
     .TxD(txd),
     .TxD_busy(ext_uart_busy),
     .TxD_start(ext_uart_start),
     .TxD_data(ext_uart_tx)
 );
 
-wire [ 3:0] arid, arlen, arcache, awid, awlen, awcache, wid, wstrb, rid, bid;
-wire [31:0] araddr, rdata, awaddr, wdata;
-wire [ 2:0] arsize, arprot, awsize, awprot;
-wire [ 1:0] arburst, arlock, awburst, awlock, rresp, bresp;
-wire arvalid, arready, rlast, rvalid, rready;
-wire awvalid, awready, wlast, wvalid, wready;
-wire bvalid, bready;
-
-mycpu_top u_cpu (
-    .aclk       (clk_cpu),
-    .aresetn    (cpu_resetn),
-    
-    .arid       (arid),
-    .araddr     (araddr),
-    .arlen      (arlen),
-    .arsize     (arsize),
-    .arburst    (arburst),
-    .arlock     (arlock),
-    .arcache    (arcache),
-    .arprot     (arprot),
-    .arvalid    (arvalid),
-    .arready    (arready),
-    
-    .rid        (rid),
-    .rdata      (rdata),
-    .rresp      (rresp),
-    .rlast      (rlast),
-    .rvalid     (rvalid),
-    .rready     (rready),
-    
-    .awid       (awid),
-    .awaddr     (awaddr),
-    .awlen      (awlen),
-    .awsize     (awsize),
-    .awburst    (awburst),
-    .awlock     (awlock),
-    .awcache    (awcache),
-    .awprot     (awprot),
-    .awvalid    (awvalid),
-    .awready    (awready),
-    
-    .wid        (wid),
-    .wdata      (wdata),
-    .wstrb      (wstrb),
-    .wlast      (wlast),
-    .wvalid     (wvalid),
-    .wready     (wready),
-    
-    .bid        (bid),
-    .bresp      (bresp),
-    .bvalid     (bvalid),
-    .bready     (bready),
-    
-    .debug_wb_pc(), .debug_wb_rf_we(), .debug_wb_rf_wnum(), .debug_wb_rf_wdata()
-);
-
-
 reg [1:0] slave_state;
 localparam S_IDLE  = 2'd0;
 localparam S_READ  = 2'd1;
 localparam S_WRITE = 2'd2;
 
-always @(posedge clk_cpu) begin
-    if (!cpu_resetn) begin
+always @(posedge clk_sram) begin
+    if (!sram_resetn) begin
         slave_state <= S_IDLE;
     end else begin
         case (slave_state)
             S_IDLE: begin
-                if (arvalid) slave_state <= S_READ;
-                else if (awvalid || wvalid) slave_state <= S_WRITE;
+                if (arvalid_sram) slave_state <= S_READ;
+                else if (awvalid_sram || wvalid_sram) slave_state <= S_WRITE;
             end
             S_READ: begin
-                if (rvalid && rready && rlast) slave_state <= S_IDLE;
+                if (rvalid_sram && rready_sram && rlast_sram) slave_state <= S_IDLE;
             end
             S_WRITE: begin
-                if (bvalid && bready) slave_state <= S_IDLE;
+                if (bvalid_sram && bready_sram) slave_state <= S_IDLE;
             end
         endcase
     end
 end
 
-wire is_read  = (slave_state == S_READ)  || (slave_state == S_IDLE && arvalid);
-wire is_write = (slave_state == S_WRITE) || (slave_state == S_IDLE && !arvalid && (awvalid || wvalid));
+wire is_read  = (slave_state == S_READ)  || (slave_state == S_IDLE && arvalid_sram);
+wire is_write = (slave_state == S_WRITE) || (slave_state == S_IDLE && !arvalid_sram && (awvalid_sram || wvalid_sram));
 
 reg rvalid_reg;
 reg [31:0] raddr_reg;
@@ -176,21 +271,21 @@ reg [7:0]  rlen_reg;
 reg [1:0]  rburst_reg;
 reg [7:0]  rbeat_cnt;
 
-assign arready = is_read && !rvalid_reg;
-wire read_fire = arvalid && arready;
+assign arready_sram = is_read && !rvalid_reg;
+wire read_fire = arvalid_sram && arready_sram;
 
-always @(posedge clk_cpu) begin
-    if (!cpu_resetn) begin
+always @(posedge clk_sram) begin
+    if (!sram_resetn) begin
         rvalid_reg <= 0;
         rbeat_cnt  <= 0;
     end else if (read_fire) begin
         rvalid_reg <= 1;
-        raddr_reg  <= araddr;
-        rid_reg    <= arid;
-        rlen_reg   <= arlen;
-        rburst_reg <= arburst;
+        raddr_reg  <= araddr_sram;
+        rid_reg    <= arid_sram;
+        rlen_reg   <= arlen_sram;
+        rburst_reg <= arburst_sram;
         rbeat_cnt  <= 8'd0;
-    end else if (rvalid && rready) begin
+    end else if (rvalid_sram && rready_sram) begin
         if (rbeat_cnt == rlen_reg) begin
             rvalid_reg <= 0;
         end else begin
@@ -204,12 +299,12 @@ always @(posedge clk_cpu) begin
     end
 end
 
-assign rvalid = rvalid_reg;
-assign rlast  = (rbeat_cnt == rlen_reg);
-assign rresp  = 2'b0;
-assign rid    = rid_reg;
+assign rvalid_sram = rvalid_reg;
+assign rlast_sram  = (rbeat_cnt == rlen_reg);
+assign rresp_sram  = 2'b0;
+assign rid_sram    = rid_reg;
 
-wire [31:0] current_raddr = read_fire ? araddr : raddr_reg;
+wire [31:0] current_raddr = read_fire ? araddr_sram : raddr_reg;
 
 reg aw_recvd, w_recvd;
 reg [31:0] awaddr_reg;
@@ -218,30 +313,30 @@ reg [ 3:0] wstrb_reg;
 reg [ 3:0] bid_reg;
 reg bvalid_reg;
 
-assign awready = is_write && !aw_recvd && !bvalid_reg;
-assign wready  = is_write && !w_recvd  && !bvalid_reg;
-wire aw_fire = awvalid && awready;
-wire w_fire  = wvalid && wready;
+assign awready_sram = is_write && !aw_recvd && !bvalid_reg;
+assign wready_sram  = is_write && !w_recvd  && !bvalid_reg;
+wire aw_fire = awvalid_sram && awready_sram;
+wire w_fire  = wvalid_sram && wready_sram;
 
-always @(posedge clk_cpu) begin
-    if (!cpu_resetn) begin
+always @(posedge clk_sram) begin
+    if (!sram_resetn) begin
         aw_recvd <= 0;
         w_recvd <= 0;
         bvalid_reg <= 0;
     end else begin
-        if (bvalid && bready) begin
+        if (bvalid_sram && bready_sram) begin
             bvalid_reg <= 0;
         end
         
         if (aw_fire) begin
             aw_recvd <= 1;
-            awaddr_reg <= awaddr;
-            bid_reg <= awid;
+            awaddr_reg <= awaddr_sram;
+            bid_reg <= awid_sram;
         end
         if (w_fire) begin
             w_recvd <= 1;
-            wdata_reg <= wdata;
-            wstrb_reg <= wstrb;
+            wdata_reg <= wdata_sram;
+            wstrb_reg <= wstrb_sram;
         end
         
         if ((aw_recvd || aw_fire) && (w_recvd || w_fire) && !bvalid_reg) begin
@@ -252,14 +347,14 @@ always @(posedge clk_cpu) begin
     end
 end
 
-assign bvalid = bvalid_reg;
-assign bresp  = 2'b0;
-assign bid    = bid_reg;
+assign bvalid_sram = bvalid_reg;
+assign bresp_sram  = 2'b0;
+assign bid_sram    = bid_reg;
 
 wire do_write = ((aw_recvd || aw_fire) && (w_recvd || w_fire) && !bvalid_reg);
-wire [31:0] current_waddr = aw_fire ? awaddr : awaddr_reg;
-wire [31:0] current_wdata = w_fire ? wdata : wdata_reg;
-wire [ 3:0] current_wstrb = w_fire ? wstrb : wstrb_reg;
+wire [31:0] current_waddr = aw_fire ? awaddr_sram : awaddr_reg;
+wire [31:0] current_wdata = w_fire ? wdata_sram : wdata_reg;
+wire [ 3:0] current_wstrb = w_fire ? wstrb_sram : wstrb_reg;
 
 wire [31:0] mem_addr = do_write ? current_waddr : current_raddr;
 wire        mem_we   = do_write;
@@ -287,8 +382,8 @@ assign ext_ram_data  =  (is_ext && mem_we) ? current_wdata : 32'bz;
 
 reg uart_dlab;
 
-always @(posedge clk_cpu) begin
-    if(reset_of_clk10M) begin
+always @(posedge clk_sram) begin
+    if(!sram_resetn) begin
         ext_uart_start <= 0;
         ext_uart_tx    <= 0;
         uart_dlab      <= 0;
@@ -306,8 +401,8 @@ always @(posedge clk_cpu) begin
     end
 end
 
-always @(posedge clk_cpu) begin
-    if(reset_of_clk10M) begin
+always @(posedge clk_sram) begin
+    if(!sram_resetn) begin
         ext_uart_avai   <= 0;
         ext_uart_buffer <= 0;
     end else begin
@@ -322,12 +417,12 @@ end
 
 wire [7:0] uart_status = {2'b00, !ext_uart_busy, 4'b0000, ext_uart_avai};
 
-assign rdata = is_base ? base_ram_data :
-               is_ext  ? ext_ram_data  :
-               is_uart ? (
-                   (mem_addr[7:2] == 6'h01) ? {4{uart_status}} : 
-                   (mem_addr[7:2] == 6'h00) ? {24'd0, ext_uart_buffer} : 32'd0
-               ) : 32'd0;
+assign rdata_sram = is_base ? base_ram_data :
+                    is_ext  ? ext_ram_data  :
+                    is_uart ? (
+                        (mem_addr[7:2] == 6'h01) ? {4{uart_status}} : 
+                        (mem_addr[7:2] == 6'h00) ? {24'd0, ext_uart_buffer} : 32'd0
+                    ) : 32'd0;
 
 assign flash_rp_n   = 1'b1;
 assign flash_vpen   = 1'b1;
