ed78ef59ed13ace648a5778d7412a6ac88ae0d39
周珈民 <3230715573@qq.com>
2026-07-26 22:09:20 +0800

    FREQ --ex

--- cpu/bp/btb.v
+++ cpu/bp/btb.v
@@ -14,27 +14,25 @@ module btb (
     input  wire [ 1:0] upd_br_type,
     input  wire [31:0] upd_target
 );
-    reg         valid      [0:1023];
-    reg  [ 1:0] entry_type [0:1023];
-    reg  [19:0] tag        [0:1023];
-    reg  [31:0] target     [0:1023];
+    reg        valid      [0:63];
+    reg  [ 1:0] entry_type [0:63];
+    reg  [23:0] tag        [0:63];
+    reg  [31:0] target     [0:63];
 
-    // 读取逻辑
-    wire [ 9:0] fetch_idx = pc[11:2];
-    wire [19:0] fetch_tag = pc[31:12];
+    wire [ 5:0] fetch_idx = pc[7:2];
+    wire [23:0] fetch_tag = pc[31:8];
     
     assign btb_hit    = valid[fetch_idx] && (tag[fetch_idx] == fetch_tag);
     assign btb_type   = entry_type[fetch_idx];
     assign btb_target = target[fetch_idx];
 
-    // 更新逻辑
-    wire [ 9:0] upd_idx = upd_pc[11:2];
-    wire [19:0] upd_tag = upd_pc[31:12];
+    wire [ 5:0] upd_idx = upd_pc[7:2];
+    wire [23:0] upd_tag = upd_pc[31:8];
 
     integer i;
     always @(posedge clk) begin
         if (~resetn) begin
-            for (i = 0; i < 1024; i = i + 1) valid[i] <= 1'b0;
+            for (i = 0; i < 64; i = i + 1) valid[i] <= 1'b0;
         end
         else if (upd_en) begin
             valid[upd_idx]      <= 1'b1;
