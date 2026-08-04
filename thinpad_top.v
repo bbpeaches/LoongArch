42f63696817a94b21f7bec4b0ce6ce16c9d149bd
周珈民 <3230715573@qq.com>
2026-08-04 21:18:27 +0800

    84

--- thinpad_top.v
+++ thinpad_top.v
@@ -145,8 +145,8 @@ reg [1:0] slave_state;
 localparam S_IDLE  = 2'd0;
 localparam S_READ  = 2'd1;
 localparam S_WRITE = 2'd2;
-localparam [2:0] READ_WAIT_CYCLES  = 3'd4;
-localparam [2:0] WRITE_WAIT_CYCLES = 3'd4;
+localparam [2:0] READ_WAIT_CYCLES  = 3'd3;
+localparam [2:0] WRITE_WAIT_CYCLES = 3'd2;
 
 always @(posedge clk_sram) begin
     if (!sram_resetn) begin
